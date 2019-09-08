737d094a1cdf1ff8b43b99bab312bf1398ae8943
TysonBHoffman <tyson@leading2lean.com>
2019-09-07 23:59:52 -0600

    Infrastructure

---
+++ configuration.py
@@ -0,0 +1,14 @@
+
+
+class Configuration():
+
+    def __init__(self):
+        self.config_file_name = 'config.json.enc'
+        self.config_file_path = '.'
+        self.encryption_key_name = 'key'
+        self.secure_json =
+
+
+    def aliases
+
+
---
+++ handlers/create_group_handler.py
@@ -0,0 +1,9 @@
+
+import re
+
+class CreateGroupHandler()
+
+
+    def __init__(self, signal, config):
+        self.alias_rgx = config['']
+        self.signal
